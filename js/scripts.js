4efbf595257d1de14f5ea1375a4e724d02c513c9
pdwkang <dwkang@uga.edu>
2016-12-23 11:26:31 -0500

    desgins added

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
 		if((!hitCounter)&&(myBet>0)){
 			$('.player-total-number').show()
 			$('.arrow').hide();
+			// $('.theWholeWorld').hide();
 			reset();
 			playerHands.push(theDeck.shift());
 			playerHands.push(theDeck.shift());
@@ -530,9 +531,9 @@ $(document).ready(function(){
 		for(let i=0; i<gridSize; i++){
 			setTimeout(function(){
 				var tempClass = '.c' + i
-				$(tempClass).toggleClass('flip')}, i*300)};
+				$(tempClass).toggleClass('flip')}, i*150)};
 		canClick = true;
-		}, 3000)
+		}, 2700)
 
 	var gridSize = 12;
 	var mgHTML = '';
@@ -590,9 +591,11 @@ $(document).ready(function(){
     		if(matchCounter===6){
 				$('.mg-contents').hide();
 				$('.circle').fadeOut();
+				$('.hideThis').show()
 				$('.theWholeWorld').fadeIn(1000);
 				clearInterval(spinSpin);
 				$('.arrow').show();
+				$('.crow').show();
 				startDrawing = true;
    				for(let i = 0; i<60; i++){
 					setTimeout(function(){
@@ -607,9 +610,11 @@ $(document).ready(function(){
 	$('.skip-button').click(function(){
 		$('.mg-contents').addClass('.flip2').hide();
 		$('.circle').fadeOut();
+		$('.hideThis').show()
 		$('.theWholeWorld').fadeIn(1000);
 		clearInterval(spinSpin);
 		$('.arrow').show();
+		$('.crow').show();
 		startDrawing = true;
 		for(let i = 0; i<50; i++){
 			setTimeout(function(){
@@ -635,70 +640,71 @@ function changeColor(){
 }
 var spinSpin = setInterval(changeColor, 300)
 
-
-
+//////////////////
+//////////////////
 // canvas
-$(document).ready(function(){
-var canvas = document.getElementById('the-canvas');
-var context = canvas.getContext('2d');
-var r = 0;
-var g = 0;
-var b = 0;
-var x = 30;
-var y = 30;
-var radius = 30;
-var endArc = 0;
-var xDirection = 1
-var yDirection = 1
-var xBallSpeed = 15
-var yBallSpeed = 16
-var rectCounter = 0
+//////////////////
+// $(document).ready(function(){
+// var canvas = document.getElementById('the-canvas');
+// var context = canvas.getContext('2d');
+// var r = 0;
+// var g = 0;
+// var b = 0;
+// var x = 30;
+// var y = 30;
+// var radius = 30;
+// var endArc = 0;
+// var xDirection = 1
+// var yDirection = 1
+// var xBallSpeed = 15
+// var yBallSpeed = 16
+// var rectCounter = 0
 
-function draw(){
-if(startDrawing){
-	if(rectCounter===10){
-	// context.clearRect(0,0,500,500);
-	rectCounter = 0
-	radius = 0}
-	context.fillStyle = '#' + r + g + b;
-	context.beginPath();
-	context.arc(x, y, radius, 0*Math.PI, Math.PI*endArc);
-	context.fill();
-	x += xBallSpeed*xDirection
-	y += yBallSpeed*yDirection
+// function draw(){
+// if(startDrawing){
+// 	if(rectCounter===10){
+// 	// context.clearRect(0,0,500,500);
+// 	rectCounter = 0
+// 	radius = 0}
+// 	context.fillStyle = '#' + r + g + b;
+// 	context.beginPath();
+// 	context.arc(x, y, radius, 0*Math.PI, Math.PI*endArc);
+// 	context.fill();
+// 	x += xBallSpeed*xDirection
+// 	y += yBallSpeed*yDirection
 	
-	if(x >= 780){
-	xDirection = -1;
-	changeColor2();
-	xBallSpeed = 19;
-	}
+// 	if(x >= 780){
+// 	xDirection = -1;
+// 	changeColor2();
+// 	xBallSpeed = 19;
+// 	}
 	
-	if(x <= 10){
-	xDirection = +1;
-	changeColor2();
-	xBallSpeed = 7}
+// 	if(x <= 10){
+// 	xDirection = +1;
+// 	changeColor2();
+// 	xBallSpeed = 7}
 	
-	if(y >= 450){
-	yDirection = -1;
-	changeColor2();
-	yBallSpeed = 22}
+// 	if(y >= 450){
+// 	yDirection = -1;
+// 	changeColor2();
+// 	yBallSpeed = 22}
 	
-	if(y <= 10){
-	yDirection = +1;
-	changeColor2();
-	yBallSpeed = 12}
-	endArc += 1;
-	rectCounter++
-	radius = (rectCounter);
-	}
-}
-var ballInterval = setInterval(draw, 45);
+// 	if(y <= 10){
+// 	yDirection = +1;
+// 	changeColor2();
+// 	yBallSpeed = 12}
+// 	endArc += 1;
+// 	rectCounter++
+// 	radius = (rectCounter);
+// 	}
+// }
+// var ballInterval = setInterval(draw, 45);
 
-function changeColor2(){
-	r = Math.floor(Math.random()*200)
-	g = Math.floor(Math.random()*22)*Math.floor(Math.random()*10)
-	b = Math.floor(Math.random()*150)
-}
+// function changeColor2(){
+// 	r = Math.floor(Math.random()*200)
+// 	g = Math.floor(Math.random()*22)*Math.floor(Math.random()*10)
+// 	b = Math.floor(Math.random()*150)
+// }
 
-$('.arrow').hide();
-})
+// $('.arrow').hide();
+// })
